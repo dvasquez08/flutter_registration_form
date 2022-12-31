afeb455ccaca9db8cc61bd3f805155c3f7532825
David Vasquez <99619761+dvasquez08@users.noreply.github.com>
2022-12-31 01:22:39 -0700

    Update main.dart
    Updated code with the layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dave_regform/regFormMobile.dart';
 import 'package:dave_regform/registrationForm.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -23,14 +24,24 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This contains the layout builder tells the app to load the desktop version of the app which is registrationForm.dart,
+  // and then to load the regFormMobile if the screen size is smaller. This makes the app more responsive
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Registration Form',
-      home: const registrationForm(),
+      home: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 800) {
+            return registrationForm();
+          } else {
+            return regFormMobile();
+          }
+        },
+      ),
     );
   }
 }
